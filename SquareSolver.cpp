7ccd3cc9d42be039fb9b3f26c4eeabe1bb7c6094
DataFIex <77386386+DataFIex@users.noreply.github.com>
2022-08-22 19:40:15 +0300

    Add the option when discriminant doesn't exist

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -6,20 +6,25 @@ int main(void)
     printf("Enter the coefficients of the square equation.\n");
     scanf("%f %f %f",&a,&b,&c);
     if(a==0)
-        printf("Cringe");
+        printf("Cringe with coefficients");
     else
     {
         d=b*b - 4*a*c;
-        if(d==0)
-            {
-                x1=(-b)/2*a;
-                printf("The solution of the equation is %.2f",x1);
-            }
+        if(d<0)
+            printf("Cringe with discriminant");
         else
             {
-                x1=(-b-sqrt(d))/(2*a);
-                x2=(-b+sqrt(d))/(2*a);
-                printf("The solutions of the equation are  %.2f and %.2f",x1,x2);
+                if(d==0)
+                    {
+                        x1=(-b)/2*a;
+                        printf("The solution of the equation is %.2f",x1);
+                    }
+                else
+                    {
+                        x1=(-b-sqrt(d))/(2*a);
+                        x2=(-b+sqrt(d))/(2*a);
+                        printf("The solutions of the equation are  %.2f and %.2f",x1,x2);
+                    }
             }
     }
     return(0);
